419c6fcc6bbaa6394d6801a4284cb93e4895ddcf
Shaun Singh <shaunsingh@Shauns-MacBook-Pro.local>
2016-02-24 16:45:46 -0800

    Added test coverage for multiplication

--- testing/test_mult.py
+++ testing/test_mult.py
@@ -3,12 +3,12 @@ import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-import multiply
+import multiply as mult
 
 
 def test_multiply1():
-    assert multiply(1, 2) == 2
+    assert mult.multiply(1, 2) == 2
 
 
 def test_multiply2():
-    assert multiply(4) == 16
+    assert mult.multiply(4) == 16
